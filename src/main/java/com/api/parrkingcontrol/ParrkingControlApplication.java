317351130558857c6260c9fcd3c4550e7a0d92d9
Scheleder <scheleder@yahoo.com>
2022-10-25 09:15:05 -0300

    first commit

---
+++ src/main/java/com/api/parrkingcontrol/ParrkingControlApplication.java
@@ -0,0 +1,20 @@
+package com.api.parrkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ParrkingControlApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ParrkingControlApplication.class, args);
+		
+		
+		
+	}
+
+}
---
+++ src/test/java/com/api/parrkingcontrol/ParrkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parrkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParrkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
